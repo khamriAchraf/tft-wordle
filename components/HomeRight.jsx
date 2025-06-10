4a6162dca41b4fbe0bd707e898dd4188b34bd7f5
khamriAchraf <achrafkhamri99@gmail.com>
2025-06-10 16:37:10 +0100

    tweaks

--- components/HomeRight.jsx
+++ components/HomeRight.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
-import { Text } from '@mantine/core';
+import { Badge, Text } from '@mantine/core';
 
 
 const HomeRight = () => {
@@ -19,6 +19,8 @@ const HomeRight = () => {
     return (
         <div className={styles.right} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <Link href="/remix-rumble">
+                <Badge className={styles.set10Badge} size="xl">Set 10</Badge>
+
                 <img
                     className={styles.background}
                     src="/images/remix-rumble-bg.jpg"
